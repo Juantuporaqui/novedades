bca8da9df7f1c421c6fd0043e66341cb7a3a68a6
Juantuporaqui <juantuporaqui@yahoo.es>
2025-06-26 10:35:22 +0200

    Update consulta.js

--- js/consulta.js
+++ js/consulta.js
@@ -3,15 +3,15 @@
 
 // --- CONFIGURACIÓN FIREBASE ---
 const firebaseConfig = {
-  // RELLENA con tu configuración real.
-  apiKey: "XXXXX",
-  authDomain: "XXXXX",
-  projectId: "XXXXX",
-  storageBucket: "XXXXX",
-  messagingSenderId: "XXXXX",
-  appId: "XXXXX"
+  apiKey: "AIzaSyDTvriR7KjlAINO44xhDDvIDlc4T_4nilo",
+  authDomain: "ucrif-5bb75.firebaseapp.com",
+  projectId: "ucrif-5bb75",
+  storageBucket: "ucrif-5bb75.appspot.com",
+  messagingSenderId: "241698436443",
+  appId: "1:241698436443:web:1f333b3ae3f813b755167e",
+  measurementId: "G-S2VPQNWZ21"
 };
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps || !firebase.apps.length) firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 // --- ELEMENTOS DOM ---
